45d366ce168c6ed3f07c6c40d8087a0338c13dde
za1goo <95066887+za1goo@users.noreply.github.com>
2022-05-20 12:29:19 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-import datetime     #Импорт модуля
+import datetime
 
-x = datetime.datetime.now()     #Вызов метода now из класса datetime
-num_day_year = (x.strftime("%j"))   #Значение текущего дня (номер дня в году)
-open_reading_file = open('holiday', encoding='utf-8').read().split('\n')[int(num_day_year)]   #Открытие файла с праздниками (чтение определённой строки)
-print(open_reading_file)    #Вывод на экран текущей даты и праздника
+x = datetime.datetime.now()
+num_day_year = (x.strftime("%j"))
+open_reading_file = open('holiday', encoding='utf-8').read().split('\n')[int(num_day_year)]
+print(open_reading_file)
 if 'нет' not in open_reading_file:
     while True:
-        question = input('Хотите узнать больше об этом празднике? (да/нет): ')  #Вопрос пользователю
+        question = input('Хотите узнать больше об этом празднике? (да/нет): ')
         if question == 'нет':
             print('Программа завершена.')
             break
         elif question == 'да':
-            x2 = datetime.datetime.now()    #Вызов метода now из класса datetime
-            num_day_year2 = (x2.strftime("%d")) #Значение текущего дня (номер дня в месяце)
-            open_reading_file2 = open('information', encoding='utf-8').read().split('\n')[int(num_day_year2)] #Открытие файла с информацией о праздниках (чтение определённой строки)
-            print(open_reading_file2)   #Вывод на экран информации о праздниках
+            x2 = datetime.datetime.now()
+            num_day_year2 = (x2.strftime("%d"))
+            open_reading_file2 = open('information', encoding='utf-8').read().split('\n')[int(num_day_year2)]
+            print(open_reading_file2)
             break
         else:
-            print('Неккоректный ввод, попробуйте снова.')
+            print('Некорректный ввод, попробуйте снова.')
             continue
